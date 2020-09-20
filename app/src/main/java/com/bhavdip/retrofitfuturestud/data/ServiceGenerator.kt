cec6d0708dff8125a4d9804edcdad0ef8bddfbe4
bhavdip <bhalodia.bhavdip@gmail.com>
2020-09-21 02:29:34 +0530

    Preparing Logging for ServiceGenerator class & Add comments   - Preparing Logging(added dependency for http logging-interceptor)   - Also added Hawk auth for future use, so code commented
      - Ref_Link : 3.2 https://futurestud.io/tutorials/retrofit-2-creating-a-sustainable-android-client

--- app/src/main/java/com/bhavdip/retrofitfuturestud/data/ServiceGenerator.kt
+++ app/src/main/java/com/bhavdip/retrofitfuturestud/data/ServiceGenerator.kt
@@ -1,23 +1,59 @@
 package com.bhavdip.retrofitfuturestud.data
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ServiceGenerator {
     companion object {
+
+        // Base Url
         private const val BASE_URL = "https://api.github.com/"
+
+        // Builder for retrofit
         private val builder = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
 
-        private val retrofit = builder.build()
+        // Retrofit object
+        private var retrofit = builder.build()
+
+        // HttpLoggingInterceptor for looging
+        private val logging: HttpLoggingInterceptor = run {
+            val httpLoggingInterceptor = HttpLoggingInterceptor()
+            httpLoggingInterceptor.apply {
+                httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            }
+        }
 
         private val httpClient = OkHttpClient.Builder()
+
         fun <S> createService(
             serviceClass: Class<S>?
         ): S {
+            if (!httpClient.interceptors().contains(logging)) {
+                httpClient.addInterceptor(logging)
+                builder.client(httpClient.build())
+                retrofit = builder.build()
+            }
             return retrofit.create(serviceClass)
         }
+
+        /* // We will use in future when Integrate Hawk Authentication
+         fun <S> createService(
+             serviceClass: Class<S>?, credentials: HawkCredentials?
+         ): S {
+             if (credentials != null) {
+                 val interceptor =
+                     HawkAuthenticationInterceptor(credentials)
+                 if (!httpClient.interceptors().contains(interceptor)) {
+                     httpClient.addInterceptor(interceptor)
+                     builder.client(httpClient.build())
+                     retrofit = builder.build()
+                 }
+             }
+             return retrofit.create(serviceClass)
+         }*/
     }
 }
